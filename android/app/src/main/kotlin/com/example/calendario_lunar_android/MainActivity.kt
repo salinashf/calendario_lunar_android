4d925faf73f50ae8f6c34a4052ba304a719244a8
Salinas Flores Henry <salinashf@gmail.com>
2025-04-09 19:09:34 -0500

    carga inicial

---
+++ android/app/src/main/kotlin/com/example/calendario_lunar_android/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.calendario_lunar_android
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'componets/CarrucelCalendar.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const CarrucelCalendarPage(titleHeader: 'Calendario Lunar'),
+    );
+  }
+}
